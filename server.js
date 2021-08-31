6a41804f42af298e0ab6a2f34aab02e144873c37
Megan <smith.megan.design@gmail.com>
2021-08-31 11:54:25 -0600

    adding post request

--- server.js
+++ server.js
@@ -15,9 +15,21 @@ var rollbar = new Rollbar({
 
 // record a generic message and send it to Rollbar
 rollbar.log("Hello world!");
+
 app.get('/', (req, res)=>{
 res.sendFile(path.join(__dirname, 'public/index.html'))
 rollbar.info('html file served successfully.')
+})
+
+app.post('/api/student', (req,res)=>{
+  let {name} = req.body
+  name=name.trim()
+
+  students.push(name)
+
+  rollbar.log('student added successfully', {author:'Scott', type: 'manual entry'})
+
+  res.status(200).send(students)
 })
 
 const port=process.env.PORT || 4545
