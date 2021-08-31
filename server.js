48f628fe80eeea1a18e5f285b80808ebca9bc8ba
Megan <smith.megan.design@gmail.com>
2021-08-31 11:18:47 -0600

    adding rollbar middleware

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const path= require('path')
+const Rollbar = require('rollbar')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var rollbar = new Rollbar({
+  accessToken: '5520cd4cf12a4fe48fa8a32e60b9ee10',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
 app.get('/', (req, res)=>{
 res.sendFile(path.join(__dirname, 'public/index.html'))
 })
